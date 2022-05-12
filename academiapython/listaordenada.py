a1622ce37d8197fb76afe00bbdca3b8b26a65dbe
LuisVallada <103458621+LuisVallada@users.noreply.github.com>
2022-05-12 19:20:05 -0300

    Update listaordenada.py

--- academiapython/listaordenada.py
+++ academiapython/listaordenada.py
@@ -2,9 +2,8 @@ def adiciona_em_ordem(paisx,distanciax,listapais):
     listapais = []
     for paisx in listapais:
         if paisx in listapais:
+            return 0
+        else:
             listapais.append(paisx)
             listapais.append(distanciax)
-        else:
-            return 0
     return listapais
-    
