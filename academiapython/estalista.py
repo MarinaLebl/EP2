1d2a04dcb4ab5c1df3cd98c3d39d0ca3e317a9bd
LuisVallada <103458621+LuisVallada@users.noreply.github.com>
2022-05-13 09:24:33 -0300

    Update estalista.py

--- academiapython/estalista.py
+++ academiapython/estalista.py
@@ -1,9 +1,9 @@
-def esta_na_lista(pais,lista):
-    res=0
-    for item in lista:
-        if pais==item:
-            res==1
-        if res==1:
-            return True
-        else:
-            return False
+def esta_na_lista(pais, lista):
+    esta = 0
+    for i in lista:
+        if pais == i[0]:
+            esta = 1
+    if esta == 1:
+        return True
+    else:
+        return False
