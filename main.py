65c3db32db1015cd2e6eecfcacf7b8b4137a18b3
MarinaLebl <marinabplebl@gmail.com>
2022-05-13 09:59:46 -0300

    add design inicial

---
+++ main.py
@@ -0,0 +1,13 @@
+#design inicial
+print(" =============================")
+print("|                             |")
+print("| Bem-vindo ao Insper Países  |")
+print("|                             |")
+print(" ======Design de Software======")
+print("\n")
+print("Comandos: ")
+print("     dica-> entra no mercado de dicas")
+print("     desisto-> desiste da rodada")
+print("     inventario-> exibe sua posição")
+
+
