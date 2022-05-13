792f39d0c92f29e12eebea44e8ddde42e29a8c28
MarinaLebl <marinabplebl@gmail.com>
2022-05-13 09:37:29 -0300

    add distancia

--- academiapython/distancia.py
+++ academiapython/distancia.py
@@ -4,7 +4,9 @@ def haversine(raio, la1_, lo1_, la2_, lo2_):
     lo1= (lo1_*math.pi)/180
     la2= (la2_*math.pi)/180
     lo2= (lo2_*math.pi)/180
-    a=math.sin((la2-la1)/2)**2
-    b=math.sin((lo2-lo1)/2)**2
-    d=2*raio*math.asin( a + (math.cos(la1)* math.cos(la2)* b))**0.5
+    a=((math.sin((la2-la1)/2))**2)
+    b=((math.sin((lo2-lo1)/2))**2)
+    c= math.cos(la1)* math.cos(la2)
+    r= math.sqrt(a+(c*b))
+    d=2*raio*math.asin(r)
     return d
