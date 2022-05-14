d840d5819ad5aee69157b7983d03f24c1b92fa8d
LuisVallada <103458621+LuisVallada@users.noreply.github.com>
2022-05-14 19:48:46 -0300

    Update jogo.py

--- academiapython/jogo.py
+++ academiapython/jogo.py
@@ -1,3 +1,7 @@
-a = input('Pressione ''1'' para iniciar o jogo: ')
-if a != '1':
+print('Bem-vindo ao Insper Países')
+
+a = input('Pressione qualquer tecla para iniciar: ')
+b = input('Menu: Pressione ''1'' para jogar, pressione ''2'' para dicas, pressione ''3'' para sair ')
+
+if b == '3':
     print('até a próxima')
