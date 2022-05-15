2330b8b1d27e75c045eb83ba6d4c00989a8bee24
LuisVallada <103458621+LuisVallada@users.noreply.github.com>
2022-05-15 15:17:08 -0300

    Update jogo.py

--- academiapython/jogo.py
+++ academiapython/jogo.py
@@ -3,13 +3,12 @@ print('Bem-vindo ao Insper Países')
 a = input('Pressione qualquer tecla para iniciar: ')
 b = input('Menu: Pressione ''1'' para jogar ou pressione ''2'' para sair ')
 
-while a != '1':
-    print('comando inválido')
+
 if b == '2':
     print('até a próxima')
-if b != '2':
+if b != '1' and b !='2':
     print('comando inválido')
-
+    
 def instrucoes(instrucao):
     print('Comandos:')
     print('dica -  entra no mercado de dicas')
