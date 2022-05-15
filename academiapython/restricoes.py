e34d4e5c0ffb0c6326fec7312457c6b2dbd72524
MarinaLebl <marinabplebl@gmail.com>
2022-05-15 17:17:54 -0300

    corrigir restricoes

--- academiapython/restricoes.py
+++ academiapython/restricoes.py
@@ -1,9 +1,17 @@
-def sorteia_letra(palavra, restrito, execs=1000):
-    test_results = {}
-    for _ in range(execs):
-        letra = sorteia_letra(palavra, restrito)
-        if letra in test_results:
-            test_results[letra] += 1
-        else:
-            test_results[letra] = 1
-    return test_results
+import random 
+def sorteia_letra(palavra, restrita):
+    palavra=palavra.lower()
+    especiais=['.', ',', '-', ';', ' ']
+    for item in restrita:
+        especiais.append(item)
+    for m in palavra:
+        if m in especiais:
+            palavra=palavra.replace(m, "")
+    if palavra=="":
+        return("")
+    letra=random.choice(palavra)
+    while letra in especiais:
+        letra=random.choice(palavra)
+    return letra
+
+print(sorteia_letra("Andorra a-Velha", ['a', 'r']))
